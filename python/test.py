9f6043ec05857cb5a661b2de5356a9a85173a463
gabe rankin <gabe.rankin@gmail.com>
2013-05-06 15:02:53 -0400

    changes to example python file

--- python/test.py
+++ python/test.py
@@ -1,10 +1,10 @@
-import volar
+import volar, pprint
 
 v = volar.Volar(base_url = 'local.platypus.com', api_key = '934bf28ae6c5575b3bb6e3e94da47cde', secret = 'JVJys$vZ-d8im2E:zLRO5UzWXd.A#V$i')
-print v.build_signature(route = 'api/client/info')
+print v.build_signature(route = 'api/client/broadcast')
 
-response = v.request(route = 'api/client/info')
+response = v.request(route = 'api/client/broadcast')
 if response == False:
 	print v.error
 else:
-	print response
+	pprint.pprint(response)
