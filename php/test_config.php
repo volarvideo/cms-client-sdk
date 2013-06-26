bc8b0d10836d016f7ad1c6f455795b768c27ee64
gabe rankin <gabe.rankin@gmail.com>
2013-06-26 09:27:01 -0400

    changing how samples are presented so they don't include location specific api keys

--- php/test_config.php
+++ php/test_config.php
@@ -1,5 +1,5 @@
 <?php
-define('VOLAR_BASE_URL', 'local.platypus.com');
-define('VOLAR_API_KEY', 'BXwwWHExORQWRN6nPj9jTzeR5QQB4F26');
-define('VOLAR_SECRET_KEY', 'TDsy[]EC<6)OtL^_Za%9Qxq\NA>RZ1C5');
+define('VOLAR_BASE_URL', 'your cms domain goes here.  typically, this will be vcloud.volarvideo.com');
+define('VOLAR_API_KEY', "your api user's api key goes here");
+define('VOLAR_SECRET_KEY', "your api user's secret key goes here");
 ?>
--- python/sample.py
+++ python/sample.py
@@ -3,12 +3,12 @@ import volar, pprint, ConfigParser
 # load settings
 try:
 	c = ConfigParser.ConfigParser()
-	c.read('test.cfg')
+	c.read('sample.cfg')	#note that this file is only for use with this script.  however, you can copy its contents and this code to use in your own scripts
 	base_url = c.get('settings','base_url')
 	api_key = c.get('settings','api_key')
 	secret = c.get('settings','secret')
 except:
-	print "couldn't read settings.  please check the values in test.cfg"
+	print "couldn't read settings.  please check the values in sample.cfg"
 	exit(1);
 
 v = volar.Volar(base_url = base_url, api_key = api_key, secret = secret)
@@ -17,27 +17,33 @@ print "-- SITES --"
 response = v.sites()
 if response == False:
 	print v.error
+	exit(1);
 else:
 	pprint.pprint(response)
 
+site_slug = response['sites'][0]['slug']
+
 print "-- BROADCASTS --"
-response = v.broadcasts({'site':'default'})
+response = v.broadcasts({'site':site_slug})
 if response == False:
 	print v.error
+	exit(1);
 else:
 	pprint.pprint(response)
 
 print "-- SECTIONS --"
-response = v.sections({'site':'default'})
+response = v.sections({'site':site_slug})
 if response == False:
 	print v.error
+	exit(1);
 else:
 	pprint.pprint(response)
 
 print "-- PLAYLISTS --"
-response = v.playlists({'site':'default'})
+response = v.playlists({'site':site_slug})
 if response == False:
 	print v.error
+	exit(1);
 else:
 	pprint.pprint(response)
 
