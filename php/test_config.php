d8116e427a8549ae82e08953d50e59af6def4a01
darren <darren@volarvideo.com>
2013-07-03 11:48:37 -0400

    Adding php unit tests -Kevin Overall <kevin.overall@volarvideo.com>

--- php/test_config.php
+++ php/test_config.php
@@ -1,5 +1,5 @@
 <?php
-define('VOLAR_BASE_URL', 'your cms domain goes here.  typically, this will be vcloud.volarvideo.com');
-define('VOLAR_API_KEY', "your api user's api key goes here");
-define('VOLAR_SECRET_KEY', "your api user's secret key goes here");
+define('VOLAR_BASE_URL', 'staging.platypusgranola.com');
+define('VOLAR_API_KEY', "8yBOLdFqu4tjyjY6Wjd2mwyjNIuC6jkW");
+define('VOLAR_SECRET_KEY', "w}EzJJVC:SwJ_!zd%U:[IY<cCQh.|TRf");
 ?>
